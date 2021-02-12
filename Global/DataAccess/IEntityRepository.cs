1583274ffb1544bea0cf92dbee0fe0b259303d81
mertsyilmaz <36952537+mertsyilmaz@users.noreply.github.com>
2021-02-13 00:09:02 +0100

    Update Global Layer

---
+++ Global/DataAccess/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Global.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Global.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        T Add(T entity);
+        T Delete(T entity);
+        T Update(T entity);
+    }
+}
