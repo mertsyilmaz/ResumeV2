e7cc3afd244369908a2dbed61c7dce82a7da2f22
mertsyilmaz <36952537+mertsyilmaz@users.noreply.github.com>
2021-02-13 00:44:50 +0100

    Add ICategoryManager and CategoryManager in Business layer

---
+++ Business/Abstract/ICategoryManager.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using Global.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICategoryManager
+    {
+        IDataResult<List<Category>> GetAll();
+        IDataResult<Category> GetById(int? categoryId);
+        IDataResult<Category> Add(Category category);
+        IDataResult<Category> Delete(Category category);
+        IDataResult<Category> Update(Category category);
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Global.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    class CategoryManager : ICategoryManager
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IDataResult<Category> Add(Category category)
+        {
+            if (category != null)
+                return new SuccessDataResult<Category>(_categoryDal.Add(category), "Category added!");
+            else
+                return new ErrorDataResult<Category>(category, "Category could not be added!");
+        }
+
+        public IDataResult<Category> Delete(Category category)
+        {
+            if (category != null)
+                return new SuccessDataResult<Category>(_categoryDal.Delete(category), "Category deleted!");
+            else
+                return new ErrorDataResult<Category>(category, "Category could not be deleted!");
+        }
+
+        public IDataResult<List<Category>> GetAll()
+        {
+            return new SuccessDataResult<List<Category>>(_categoryDal.GetAll());
+        }
+
+        public IDataResult<Category> GetById(int? categoryId)
+        {
+            if (categoryId != null)
+                return new SuccessDataResult<Category>(_categoryDal.Get(c => c.Id == categoryId));
+            else
+                return new ErrorDataResult<Category>("Category not found!");
+        }
+
+        public IDataResult<Category> Update(Category category)
+        {
+            if (category != null)
+                return new SuccessDataResult<Category>(_categoryDal.Update(category), "Category updated!");
+            else
+                return new ErrorDataResult<Category>(category, "Category could not be updated!");
+        }
+    }
+}
